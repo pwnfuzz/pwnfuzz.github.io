cf9f43e5975ae432a20f38878f2ba5edcfedd3cf
D4mianWayne <dubeyayushmanrobin@gmail.com>
2025-12-01 15:19:26 +0530

    UI improvements

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,10 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://pwnfuzz.github.io',
+  markdown: {
+    shikiConfig: {
+      theme: 'github-light', // Use GitHub's light theme for syntax highlighting
+      wrap: true,
+    },
+  },
 });
