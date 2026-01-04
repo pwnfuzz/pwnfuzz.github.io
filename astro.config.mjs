9a490bb4cb97ed841642cd85982bb7e9e149cbf5
D4mianWayne <dubeyayushmanrobin@gmail.com>
2026-01-04 15:24:38 +0530

    Added breakdown for CVE-2025-68615

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import mermaid from 'astro-mermaid';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://pwnfuzz.github.io',
+  integrations: [mermaid()],
   markdown: {
     shikiConfig: {
       theme: 'github-dark',
       wrap: true,
+      // @ts-ignore
+      langs: ['c'],
     },
   },
 });
